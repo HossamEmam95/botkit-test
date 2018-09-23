f930d6174bef9f372ed9b101927d466f67b5900b
hossam <hossamsalahemam@gmail.com>
2018-09-23 11:14:06 +0200

    test

--- incoming_webhooks.js
+++ incoming_webhooks.js
@@ -1,76 +1,38 @@
 var debug = require('debug')('botkit:incoming_webhooks');
 
-let express = require('express'),
-    bodyParser = require('body-parser'),
-    app = express();
+var webserver = express();
+webserver.use(bodyParser.json());
+webserver.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+webserver.listen(3000, null, function() {
 
-app.get('/', (req, res) => res.send('Manual Webhook For Facbbok'));
+    console.log('Express webserver configured and listening at http://localhost:' +  3000);
 
-app.get('/facebook/receive', (req, res) => {
-
-    // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = "Cairo2018";
-
-    // Parse the query params
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
-
-    // Checks if a token and mode is in the query string of the request
-    if (mode && token) {
-
-        // Checks the mode and token sent is correct
-        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-
-            // Responds with the challenge token from the request
-            console.log('WEBHOOK_VERIFIED');
-            res.status(200).send(challenge);
-
-        } else {
-            // Responds with '403 Forbidden' if verify tokens do not match
-            res.sendStatus(403);
-        }
-    }
 });
 
-app.post('/facebook/receive', (req, res) => {
+    webserver.post('/facebook/receive', function(req, res) {
 
-    let body = req.body;
-    console.log("post request ")
+        // NOTE: we should enforce the token check here
 
-    if (body.object === 'page') {
+        // respond to Slack that the webhook has been received.
+        res.status(200);
+        res.send('ok');
 
-        // Iterates over each entry - there may be multiple if batched
-        body.entry.forEach(function(entry) {
+        console.log(req.body);
 
-            // Gets the message. entry.messaging is an array, but
-            // will only ever contain one message, so we get index 0
-            let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
+        // Now, pass the webhook into be processed
 
-            // Get the sender PSID
-            let sender_psid = webhook_event.sender.id;
-            console.log('Sender PSID: ' + sender_psid);
 
-            // Check if the event is a message or postback and
-            // pass the event to the appropriate handler function
-            if (webhook_event.message) {
-                console.log(webhook_event.message)
-            } else if (webhook_event.postback) {
-                console.log(webhook_event.postback)
+    });
+
+    webserver.get('/facebook/receive', function(req, res) {
+      console.log("get request");
+        if (req.query['hub.mode'] == 'subscribe') {
+            if (req.query['hub.verify_token'] == "Cairo2018") {
+                res.send(req.query['hub.challenge']);
+            } else {
+                res.send('OK');
             }
-        });
-
-        // Returns a '200 OK' response to all requests
-        res.status(200).send('EVENT_RECEIVED');
-    } else {
-        // Returns a '404 Not Found' if event is not from a page subscription
-        res.sendStatus(404);
-    }
-
-});
+        }
+    });
